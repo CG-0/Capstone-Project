c4c49117680081bf248e70b71cdcc8448a811d9a
Chaska Gonzales <chaskagonzales@mail.weber.edu>
2026-02-08 20:36:41 -0700

    "Basis for BaseCommand and other components"

--- capstoneproject/content/management/commands/seed_db.py
+++ capstoneproject/content/management/commands/seed_db.py
@@ -1,5 +1,6 @@
 import csv
 from django.core.management.base import BaseCommand
+from content.models import Seed
 
 class Command(BaseCommand):
     help = "Import the records from the data.csv file to the primary model"
@@ -9,6 +10,22 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         csv_file = options['csv_file']
-        print(csv_file)
+        try:
+            with open(csv_file, 'r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    seed, created = Seed.objects.get_or_create(
+                        name=row['name'],
+                        genus=row['genus'],
+                        species=row['species'],
+                        seed_type='HYBRID',
+                        continent=row['continent']
+                    )
+                    if created:
+                        print("It was a succes!")
+                    else:
+                        print("Oops... Something went wrong")
+                self.stdout.write(self.style.SUCCESS("Import was succesful!"))
 
-        self.stdout.write(self.style.SUCCESS("Import was succesful!"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR("An error ocurred: {e}"))
