97db1fa4852987589936833d78b3744d84d9c7b5
Chaska Gonzales <chaskagonzales@mail.weber.edu>
2026-02-08 19:52:47 -0700

    "Simple BaseCommand adjusted"

--- capstoneproject/content/management/commands/seed_db.py
+++ capstoneproject/content/management/commands/seed_db.py
@@ -3,3 +3,12 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = "Import the records from the data.csv file to the primary model"
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', type=str)
+    
+    def handle(self, *args, **options):
+        csv_file = options['csv_file']
+        print(csv_file)
+
+        self.stdout.write(self.style.SUCCESS("Import was succesful!"))
