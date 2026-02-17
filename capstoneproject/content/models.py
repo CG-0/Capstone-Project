cc8e1fc42377203f11a3e5e6f1cd11bb31bc72a4
Chaska Gonzales <chaskagonzales@mail.weber.edu>
2026-02-17 13:19:30 -0700

    "Small corrections were needed"

--- capstoneproject/content/models.py
+++ capstoneproject/content/models.py
@@ -4,10 +4,20 @@ from django.urls import reverse
 
 # Create your models here.
 class Seed(models.Model):
+    HYBRID = "HYB"
+    OPEN_POLLINATED = "OP"
+    HEIRLOOM = "HE"
+
+    SEED_TYPE_CHOICES = {
+        HYBRID: 'Hybrid',
+        OPEN_POLLINATED: 'Open-Pollinated',
+        HEIRLOOM: 'Heirloom',
+    }
+
     name = models.CharField(max_length=100)
     genus = models.CharField(max_length=80)
     species = models.CharField(max_length=150)
-    seed_type = models.CharField(max_length=15)
+    seed_type = models.CharField(max_length=3, choices=SEED_TYPE_CHOICES, default=HYBRID)
     continent = models.CharField(max_length=20)
     slug = models.SlugField(max_length=500, null=True, db_index=True, help_text="SEO-friendly URLs")
 
